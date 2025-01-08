d528b49d2cef3070cb424901c0b7ebcaa83dedf6
JiveOff <antoine@jiveoff.fr>
2025-01-08 16:49:43 +0100

    feat(#9): add domain

--- src/main/java/net/efrei/springrag/web/rest/SampleResource.java
+++ src/main/java/net/efrei/springrag/web/rest/SampleResource.java
@@ -1,5 +1,6 @@
 package net.efrei.springrag.web.rest;
 
+import net.efrei.springrag.web.dto.Sample;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,4 +13,8 @@ public class SampleResource {
         return "Hello " + value + "!";
     }
 
+    @GetMapping("/samples/dto/{value}")
+    public Sample helloDto(@PathVariable(value = "value", required = false) final String value) {
+        return new Sample(value);
+    }
 }
